f4f77dc9028d0da44d5b046e67da89c680077fe4
prince shukla <princeshukla3264@gmail.com>
2025-09-10 16:18:10 +0530

    create cart Schema

--- src/models/cart.models.js
+++ src/models/cart.models.js
@@ -1,7 +1,22 @@
 import mongoose, { Schema } from "mongoose";
 
 const cartSchema = new Schema({
-
+    userId:{
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required:true,
+    },
+    productId:{
+        type: mongoose.Schema.ObjectId,
+        ref:"Product",
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    }
+},
+{
+    timestamps:true,
 })
-
 export default Cart = mongoose.model("Cart", cartSchema)
